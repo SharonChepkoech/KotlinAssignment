d08cc16363254acdc3f8adf3fefc012dc811682a
Sharon Chepkoech <sharoncchepkoech@gmail.com>
2022-02-12 07:41:34 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var name = "SharonChepkoech"
+    println(name)
+    var age = "69"
+    println(age)
+    var phonenumber = "0837373"
+    println(phonenumber)
+    var weight =(87)
+    println(weight)
+    val iskenyan: Boolean=true
+    val isnotkenyan: Boolean=false
+    println(iskenyan)//output true
+    println(isnotkenyan)//output false
+}
